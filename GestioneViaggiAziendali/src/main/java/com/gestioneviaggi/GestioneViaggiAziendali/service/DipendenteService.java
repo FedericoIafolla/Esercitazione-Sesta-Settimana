02aa01b91d827757840dce872d5cb6a03b648354
FedericoIafolla <iafollafederico@gmail.com>
2024-11-01 16:50:40 +0100

    Sistemate le repository

---
+++ GestioneViaggiAziendali/src/main/java/com/gestioneviaggi/GestioneViaggiAziendali/service/DipendenteService.java
@@ -0,0 +1,32 @@
+package com.gestioneviaggi.GestioneViaggiAziendali.service;
+
+import com.gestioneviaggi.GestioneViaggiAziendali.model.Dipendente;
+import com.gestioneviaggi.GestioneViaggiAziendali.repository.DipendenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DipendenteService {
+
+    @Autowired
+    private DipendenteRepository dipendenteRepository;
+
+    public List<Dipendente> findAll() {
+        return dipendenteRepository.findAll();
+    }
+
+    public Optional<Dipendente> findById(Long id) {
+        return dipendenteRepository.findById(id);
+    }
+
+    public Dipendente save(Dipendente dipendente) {
+        return dipendenteRepository.save(dipendente);
+    }
+
+    public void deleteById(Long id) {
+        dipendenteRepository.deleteById(id);
+    }
+}
---
+++ GestioneViaggiAziendali/src/main/java/com/gestioneviaggi/GestioneViaggiAziendali/service/ViaggioService.java
@@ -0,0 +1,34 @@
+package com.gestioneviaggi.GestioneViaggiAziendali.service;
+
+import com.gestioneviaggi.GestioneViaggiAziendali.model.Viaggio;
+import com.gestioneviaggi.GestioneViaggiAziendali.repository.ViaggioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ViaggioService {
+
+    @Autowired
+    private ViaggioRepository viaggioRepository;
+
+    public List<Viaggio> findAll() {
+        return viaggioRepository.findAll();
+    }
+
+    public Optional<Viaggio> findById(Long id) {
+        return viaggioRepository.findById(id);
+    }
+
+    public Viaggio save(Viaggio viaggio) {
+        return viaggioRepository.save(viaggio);
+    }
+
+    public Viaggio updateStato (Long id, Viaggio.StatoViaggio nuovoStato) {
+        Viaggio viaggio = viaggioRepository.findById(id).orElseThrow();
+        viaggio.setStato(nuovoStato);
+        return viaggioRepository.save(viaggio);
+    }
+}
