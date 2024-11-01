161e8c353e256f6653a81f54667f4177780a5292
FedericoIafolla <iafollafederico@gmail.com>
2024-11-01 16:15:00 +0100

    Creazione repository

---
+++ GestioneViaggiAziendali/src/main/java/com/gestioneviaggi/GestioneViaggiAziendali/repository/PrenotazioneRepository.java
@@ -0,0 +1,7 @@
+package com.gestioneviaggi.GestioneViaggiAziendali.repository;
+
+import com.gestioneviaggi.GestioneViaggiAziendali.model.Prenotazione;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+}
